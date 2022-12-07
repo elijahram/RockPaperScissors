6d6b952a67e9133be36a42d786f054774d9883f9
eLoyal <elijahbramos@gmail.com>
2022-12-06 23:14:19 -0600

    added player selection, computer selection, and rps result variables

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 let choices = [Rock, Paper, Scissors];
 let selection = choices[Math.floor(Math.random()*choice.length)];
+let playerSelection;
+let computerSelection;
+let rpsResult;
+
+function computerSelection(arr) {
+
+}
