e7d95468c92f0e756aeeac72b40c719e2987c9e1
eLoyal <elijahbramos@gmail.com>
2022-12-07 18:39:11 -0600

    Created comp select randomizer, winner check, and linked both to button selection with alert

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-let choices = [Rock, Paper, Scissors];
-let selection = choices[Math.floor(Math.random()*choice.length)];
-let playerSelection;
-let computerSelection;
-let rpsResult;
+document.getElementById("rock").addEventListener('click', () => {
+    const compSelect = computerSelection();
+    const playerSelect = "ROCK";
+    const rpsResult = checkWinner(playerSelect, compSelect);
+    alert(rpsResult);
+});
+document.getElementById("paper").addEventListener('click', () => {
+    const compSelect = computerSelection();
+    const playerSelect = "PAPER";
+    const rpsResult = checkWinner(playerSelect, compSelect);
+    alert(rpsResult);
+});
+document.getElementById("scissors").addEventListener('click', () => {
+    const compSelect = computerSelection();
+    const playerSelect = "SCISSORS";
+    const rpsResult = checkWinner(playerSelect, compSelect);
+    alert(rpsResult);
+});
 
-function computerSelection(arr) {
-
+function computerSelection() {
+    let choices = ["ROCK", "PAPER", "SCISSORS"];
+    let selection = choices[Math.floor(Math.random()*choices.length)];
+    return selection;
+}
+function checkWinner(playerSelect, compSelect) {
+    if (playerSelect === compSelect) {
+        return "It's a Draw!";
+    } else if (compSelect === "ROCK") {
+        return (playerSelect === "PAPER") ? "You win!" : "You Lose!"
+    } else if (compSelect === "PAPER") {
+        return (playerSelect === "SCISSORS") ? "You win!" : "You Lose!"
+    } else if (compSelect === "SCISSORS") {
+        return (playerSelect === "ROCK") ? "You win!" : "You Lose!"
+    }
 }
